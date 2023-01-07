9bec5364b16df1ed731596a9616d4a491242dece
Benjamin Lawson <banjoduman@gmail.com>
2023-01-07 15:42:05 -0500

    finsihed pool loottable functionaility

--- games/benjamin/genetics_txt/pools.py
+++ games/benjamin/genetics_txt/pools.py
@@ -4,28 +4,7 @@ class Entry:
     def __init__(self,value,weight=int) -> None:
         self.weight = weight
         self.value = value
-
-pool = [
-    Entry(value=1,weight=1),
-    Entry(value=2,weight=1),
-    Entry(value=3,weight=1),
-    Entry(value=4,weight=1),
-    Entry(value=5,weight=1),
-    Entry(value=6,weight=1),
-    Entry(value=7,weight=10),
-    Entry(value=8,weight=1),
-    Entry(value=9,weight=1),
-    Entry(value=10,weight=1),
-]
-
-total_wieght = 0
-for i in pool:
-    total_wieght += i.weight
-
-role = random.randint(1,total_wieght)
-
-index_value = 0
-
+        
 def select_from_pool(pool,roles):
     total_wieght = 0
     for i in pool:
@@ -41,5 +20,46 @@ def select_from_pool(pool,roles):
             # and role < (index_value+(pool[i+1].weight)):
                 items.append(item.value)
                 break
-    return items
-print((select_from_pool(pool,5)))
+    one_list_items = []
+    for i in range(len(items)):
+        if type(items[i]) == list:
+            for ii in items[i]:
+                one_list_items.append(ii)
+        else:
+            one_list_items.append(items[i])
+
+    return one_list_items
+
+pool_1_5 = [
+    Entry(value=1,weight=1),
+    Entry(value=2,weight=1),
+    Entry(value=3,weight=1),
+    Entry(value=4,weight=1),
+    Entry(value=5,weight=10)
+]
+pool_6_10= [
+    Entry(value=6,weight=1),
+    Entry(value=7,weight=1),
+    Entry(value=8,weight=1),
+    Entry(value=9,weight=1),
+    Entry(value=10,weight=10)
+]
+
+pool_11_15= [
+    Entry(value=11,weight=1),
+    Entry(value=12,weight=1),
+    Entry(value=13,weight=1),
+    Entry(value=14,weight=1),
+    Entry(value=15,weight=10)
+]
+
+pool_1_15 = [
+    Entry(value=select_from_pool(pool_1_5,5),weight=5),
+    Entry(value=select_from_pool(pool_6_10,5),weight=3),
+    Entry(value=select_from_pool(pool_11_15,5),weight=10),
+]
+#weapon functions
+#armor functions
+#food items
+
+print(select_from_pool(pool_1_15,5))
